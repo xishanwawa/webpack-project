8a586bd7b2c5ec5378de67fd736d600813b4acfc
xishanwawa <654189842@qq.com>
2016-11-28 18:21:36 +0800

    ok

--- app/Greeter.js
+++ app/Greeter.js
@@ -10,12 +10,22 @@
 import React, {Component} from 'react'
 import config from './config.json';
 import styles from './Greeter.css';//导入
+import data from './Mock/index.js'
 
 class Greeter extends Component{
   render() {
+    let list = '';
+    debugger
+    list = data.list.map(function(item, i){
+       return <p className = "tr" key = {i}>
+        <span className = "td">{item.id}</span>
+        <span className = "td">{item.string}</span>
+       </p> 
+    });
     return (
       <div className={styles.root} >
         {config.greetText}
+        {list}
       </div>
     );
   }
